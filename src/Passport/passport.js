b30a0ad99bf8ee8cdc6e833c389b6b5f93a4729e
TBBLuxari <ardilax.mtb@gmail.com>
2022-08-18 15:53:43 -0500

    notificaciones agendadas con mensajes

--- src/Passport/passport.js
+++ src/Passport/passport.js
@@ -42,10 +42,10 @@ passport.use('local-singup', new LocalStrategy({
         newNoti.titulo  ="Titulo";
         newNoti.mensaje ="Mensaje";
         newNoti.link ="Link";
-        newNoti.NA1={TNA1:"" ,MNA1:"" ,LNA1:""};
-        newNoti.NA2={TNA2:"" ,MNA2:"" ,LNA2:""};
-        newNoti.NA3={TNA3:"" ,MNA3:"" ,LNA3:""};
-        newNoti.NA4={TNA4:"" ,MNA4:"" ,LNA4:""};
+        newNoti.NA1={TNA1:"1" ,MNA1:"2" ,LNA1:"3"};
+        newNoti.NA2={TNA2:"4" ,MNA2:"5" ,LNA2:"6"};
+        newNoti.NA3={TNA3:"7" ,MNA3:"8" ,LNA3:"9"};
+        newNoti.NA4={TNA4:"10" ,MNA4:"11" ,LNA4:"12"};
         await newUser.save();
         await newNoti.save();
         done(null,newUser);
